1b2c2e7884f011f25fa1a5db7c06680aa275701c
Kim Gray <kim.lc.gray@gmail.com>
2020-11-15 22:11:42 +0000

    added social buttons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "../styles/About.css";
+import { BiEnvelope } from "react-icons/bi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 class About extends Component {
   state = {};
@@ -7,24 +10,49 @@ class About extends Component {
     return (
       <div className="welcomeText">
         <h1 classname="nameKg">Kim Gray</h1>
+        <div className="social">
+          <a href="mailto:kim.lc.gray@gmail.com">
+            <BiEnvelope id="icon" />
+          </a>
+          <a href="mailto:kim.lc.gray@gmail.com">
+            <FaLinkedinIn id="icon" />
+          </a>
+          <a href="mailto:kim.lc.gray@gmail.com">
+            <AiOutlineInstagram id="icon" />
+          </a>
+        </div>
         <p className="aboutText">
           <span className="aboutLines">
-            By day, I am a Programme Manager at Code First Girls. I am
-            London-based, but currently working remotely. Supporting diversity
-            and inclusion in tech by driving engagement through programmes and
-            events at Code First Girls. Learning languages of modern technology
-            after having completed a Web Development Bootcamp (Wild Code School,
-            Russell Code School). Currently building an app
-            (https://libertybellapp.com/ - coming to app stores near you very
-            soon) and working on personal projects. Creating bespoke watercolour
-            illustrations for cards and invitations.
+            By day, I support diversity and inclusion in tech by driving
+            engagement through programmes and events as Programme Manager at{" "}
+            <a
+              href="https://codefirstgirls.org.uk/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Code First Girls
+            </a>
+            .
           </span>
           <span className="aboutLines">
-            By night, I am a learner web developer and designer, watercolour
-            illustrator and occasional sewist.{" "}
+            By night, I am a learner web designer and developer, watercolour
+            illustrator and occasional sewist.
           </span>
           <span className="aboutLines">
-            I also collect baskets (yep, woven baskets) and wooden spoons (yep,
+            I completed a Web Development course earlier this year and continue
+            to code with my coding group. Led by our previous instructor, we are
+            currently building an{" "}
+            <a
+              href="https://libertybellapp.com/ "
+              target="_blank"
+              rel="noreferrer"
+            >
+              app{" "}
+            </a>{" "}
+            (coming to app stores near you very soon).
+          </span>
+          <span className="aboutLines">
+            I also collect baskets (yes, woven baskets) and wooden spoons (yes,
             wooden spoons for cooking).
           </span>
         </p>
